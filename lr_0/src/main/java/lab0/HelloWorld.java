e20ad6285ac30318b3a1b284e93019776f54b0ba
Trehubov_D <daniltregybov627@gmail.com>
2024-04-15 15:28:09 +0300

    lr_0

--- lr_0/src/main/java/lab0/HelloWorld.java
+++ lr_0/src/main/java/lab0/HelloWorld.java
@@ -2,7 +2,7 @@ package lab0;
 
 public class HelloWorld {
         public static void main(String[] args) {
-            System.out.println("Hello, world!");
+            System.out.println("Hello, world!.");
         }
     }
 
